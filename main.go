07de02fb3b6959898c936ddf009c0795dec0a418
HacDan <david.mcclellan.p@gmail.com>
2023-05-06 14:30:15 -0400

    feat(main.go): added ability to define pad character
    pad character can now be defined. defaults to a space character

--- main.go
+++ main.go
@@ -12,41 +12,54 @@ type Message struct {
 	Padded string
 }
 
-func rightPad(s string, p string) []byte {
+func rightPad(s string, p string, c string) []byte {
 	pad, _ := strconv.Atoi(p)
+	var padder string
+
+	if len(c) > 1 {
+		padder = c
+	} else {
+		padder = " "
+	}
 
 	if pad < 1 {
 		message := &Message{Padded: s}
 		response, _ := json.Marshal(message)
 		return response
 	}
-	padded := s + strings.Repeat("_", pad)
-	message := &Message{Padded: padded}
+	message := &Message{Padded: s + strings.Repeat(padder, pad)}
 	response, _ := json.Marshal(message)
 	return response
 }
 
-func leftPad(s string, p string) []byte {
+func leftPad(s string, p string, c string) []byte {
 	pad, _ := strconv.Atoi(p)
+	var padder string
+
+	if len(c) > 1 {
+		padder = c
+	} else {
+		padder = " "
+	}
 
 	if pad < 1 {
 		message := &Message{Padded: s}
 		response, _ := json.Marshal(message)
 		return response
 	}
-	padded := strings.Repeat("_", pad) + s
-	message := &Message{Padded: padded}
+	message := &Message{Padded: strings.Repeat(padder, pad) + s}
 	response, _ := json.Marshal(message)
 	return response
 }
 
-func paas(w http.ResponseWriter, r *http.Request) {
+func laas(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application,json")
 	switch r.Method {
 	case "GET":
 		s := r.URL.Query().Get("string")
 		pad := r.URL.Query().Get("pad")
-		response := leftPad(s, pad)
+		c := r.URL.Query().Get("padChar")
+		response := leftPad(s, pad, c)
 
 		w.WriteHeader(http.StatusOK)
 		w.Write(response)
@@ -62,7 +75,8 @@ func raas(w http.ResponseWriter, r *http.Request) {
 	case "GET":
 		s := r.URL.Query().Get("string")
 		pad := r.URL.Query().Get("pad")
-		response := rightPad(s, pad)
+		c := r.URL.Query().Get("padChar")
+		response := rightPad(s, pad, c)
 
 		w.WriteHeader(http.StatusOK)
 		w.Write(response)
@@ -74,7 +88,7 @@ func raas(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/raas", raas)
-	http.HandleFunc("/paas", paas)
+	http.HandleFunc("/laas", laas)
 
 	log.Fatal(http.ListenAndServe(":1337", nil))
 }
